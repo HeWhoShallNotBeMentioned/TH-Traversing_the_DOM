3128937150f00a8de10abf55009ea31b4f933162
Chris Underwood <cunderwoodmn@gmail.com>
2017-03-30 02:26:52 -0700

    add background color to first and last elements.

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const listUl = listDiv.querySelector('ul');
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const lis = listUl.children;
+const firstListItem = listUl.firstElementChild;
+const lastListItem = listUl.lastElementChild;
+
+firstListItem.style.backgroundColor = 'lightSteelBlue';
+lastListItem.style.backgroundColor = 'salmon';
 
 function attachListItemButtons (li) {
   let up = document.createElement('button');
